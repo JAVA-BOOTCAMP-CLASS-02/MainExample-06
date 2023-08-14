ad9955039b5c25a3b6d0b2e4a314e1254a7ce746
andresaramos <andres.alberto.ramos@gmail.com>
2023-08-14 15:42:50 -0300

    first commit

---
+++ src/MainExample.java
@@ -0,0 +1,53 @@
+
+public class MainExample {
+
+	public void cantidadDeParametrosDesconocida(String ... p) {
+		int count = 0;
+		for (String arg : p) {
+			if (count == 0) {
+				System.out.print("{");
+			} else {
+				System.out.print(", ");
+			}
+			
+			System.out.print(arg);
+			count++;
+
+		}
+		
+		System.out.println("}");
+		
+	}
+
+	public static void main(String[] args) {
+
+		int numArgs = args.length;
+		
+		if (numArgs == 0) {
+			System.out.println("Usted no hay ingresado parametros!");
+		} else {
+			System.out.println("Usted ha ingresado " + numArgs + " parametros:");
+			int count = 0;
+			for (String arg : args) {
+				if (count == 0) {
+					System.out.print("{");
+				} else {
+					System.out.print(", ");
+				}
+				
+				System.out.print(arg);
+				count++;
+
+			}
+			
+			System.out.println("}");
+			
+			if (numArgs == 3) {
+				MainExample me = new MainExample();
+				me.cantidadDeParametrosDesconocida(args[0], args[1], args[2]);
+			}
+		}
+		
+	}
+
+}
